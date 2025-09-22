1f527933db58c28300dbe079a4efa1997a26d9f0
Godmode-n1tr0 <130226927+Godmode-n1tr0@users.noreply.github.com>
2025-09-22 13:18:35 +0500

    Add files via upload

--- pract3_shengals/pract3_shengals.cpp
+++ pract3_shengals/pract3_shengals.cpp
@@ -3,71 +3,67 @@
 using namespace std;
 
 union Extra {
-    int year;         
-    char owner[50];   
+    int year;
+    char owner[50];
 };
 
 struct Housing {
-    int type;         
-    int area;         
-    Extra info;       
+    int type;
+    int ownership;    // 1 - частный, 2 - государственный
+    int area;
+    Extra info;
 };
 
 int main() {
-    setlocale(0, "rus"); 
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-    const int N = 100;     
-    Housing arr[N];        
-    int n;                 
+    const int N = 100;
+    Housing arr[N];
+    int n;
 
     cout << "Введите количество записей: ";
     cin >> n;
 
-    int minArea;           // минимальная площадь для отбора
+    int minArea;
     cout << "Введите минимальную площадь: ";
     cin >> minArea;
 
     for (int i = 0; i < n; i++) {
         cout << "\nЗапись " << i + 1 << ":\n";
         cout << "Тип (1 - квартира, 2 - дом): ";
-        cin >> arr[i].type;   
+        cin >> arr[i].type;
+        cout << "Собственность (1 - частная, 2 - государственная): ";
+        cin >> arr[i].ownership;
         cout << "Общая площадь: ";
-        cin >> arr[i].area;   
+        cin >> arr[i].area;
 
         if (arr[i].type == 1) {
             cout << "Год сдачи: ";
-            cin >> arr[i].info.year; 
+            cin >> arr[i].info.year;
         }
         else {
             cout << "Фамилия владельца: ";
-            cin >> arr[i].info.owner; 
+            cin >> arr[i].info.owner;
         }
     }
 
-    char owners[N][50];    // массив фамилий отобранных домов
-    int count = 0;         
+    char owners[N][50];
+    int count = 0;
 
     for (int i = 0; i < n; i++) {
-        if (arr[i].type == 2 && arr[i].area >= minArea) { // проверка типа и площади
-            int k = 0;
-            while (arr[i].info.owner[k] != '\0') {
-                owners[count][k] = arr[i].info.owner[k]; // копирование фамилии
-                k++;
-            }
-            owners[count][k] = '\0';
+        if (arr[i].type == 2 && arr[i].ownership == 1 && arr[i].area >= minArea) {
+            strcpy_s(owners[count], arr[i].info.owner);
             count++;
         }
     }
 
-    // Сортировка пузырьком
     for (int i = 0; i < count - 1; i++) {
         for (int j = 0; j < count - i - 1; j++) {
             if (strcmp(owners[j], owners[j + 1]) > 0) {
                 char temp[50];
-                strcpy(temp, owners[j]);
-                strcpy(owners[j], owners[j + 1]);
-                strcpy(owners[j + 1], temp);
+                strcpy_s(temp, owners[j]);
+                strcpy_s(owners[j], owners[j + 1]);
+                strcpy_s(owners[j + 1], temp);
             }
         }
     }
@@ -78,7 +74,7 @@ int main() {
     }
     else {
         for (int i = 0; i < count; i++) {
-            cout << owners[i] << endl; // вывод фамилии
+            cout << owners[i] << endl;
         }
     }
 
